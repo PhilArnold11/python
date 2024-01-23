44ebcd1d168b6ef5c3e95068f74ce12f279fa5f3
PhilArnold11 <157064792+PhilArnold11@users.noreply.github.com>
2024-01-23 17:12:49 -0600

    Add files via upload

---
+++ joke.py
@@ -0,0 +1,10 @@
+# Knock-Knock Joke Program
+print("Knock, knock.")
+print("Who's there?")
+print("Mikey.")
+print("Mikey who?")
+print("Mikey doesn't work, can you let me in?")
+print()
+
+# As one line of code using escape characters.
+print("Knock, knock. \nWho's there?\nMikey.\nMikey who?\nMikey doesn't work, can you let me in?")
