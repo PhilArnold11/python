6aca68001ac8a6edfc142bda240ff8ad6de9ad41
PhilArnold11 <157064792+PhilArnold11@users.noreply.github.com>
2024-01-25 10:49:28 -0600

    Add files via upload

---
+++ Arnold_PE1_2.2_Assignment.py
@@ -0,0 +1,22 @@
+
+# This is a Python program to demonstrate different data types
+
+# Literal
+x = 11
+print("Literal:", x)
+
+# Integer
+y = -11
+print("Integer:", y)
+
+# Float
+z = 13.11
+print("Float:", z)
+
+# String
+name = "Phil"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
