e2ee3fb8a99fbb7d8fef43bfe15bc54182c24bb2
PhilArnold11 <157064792+PhilArnold11@users.noreply.github.com>
2024-02-18 22:16:42 -0600

    Update 3.6.py

--- 3.6.py
+++ 3.6.py
@@ -1,9 +1,8 @@
-seats = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-print('Available Seats: ', seats)
-seats_purchased = '2'
+seats = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']
+seats_purchased = '50'
 while seats_purchased !='0':
     for seat in seats:
-        print('Available Seats: ', seats)
+        print('Available Seats: ', seat)
     seats_purchased = input('\nPlease enter the seat you would like to purchase from the list of available seats (Enter 0 when complete):  ')
     if seats_purchased in seats:
         seats.remove(seats_purchased)
