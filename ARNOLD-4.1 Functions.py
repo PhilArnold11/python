c1a9ee6ece75647f336a114f6a4afa7bc2007002
PhilArnold11 <157064792+PhilArnold11@users.noreply.github.com>
2024-02-22 23:08:32 -0600

    Add files via upload

---
+++ ARNOLD-4.1 Functions.py
@@ -0,0 +1,10 @@
+def square(side):#define the function and the parameter
+    area = side * side #calculate the area of a square
+    print("The area of the square is " + str(area) + " square units.")#Print function to display result/convert the numerical result to a string
+
+def circle(radius):#define the function and the parameter
+    area = 3.14 * radius * radius #calculate the area of a circle
+    print("The area of the square is " + str(area) + "square units." )#Print function to display result/convert the numerical result to a string
+
+square(10)#test function by calling
+circle(20)#test function by calling
