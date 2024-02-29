c0fadd794d7882cdc58c3ba7eb4ecf4946db7dbe
PhilArnold11 <157064792+PhilArnold11@users.noreply.github.com>
2024-02-28 21:16:50 -0600

    Add files via upload

---
+++ ARNOLD-4.2 How functions communicate with their environment.py
@@ -0,0 +1,29 @@
+"""
+The itsy bitsy spider
+crawled up the water spout.
+Down came the rain,
+and washed the spider out.
+Out came the sun,
+and dried up all the rain,
+and the itsy bitsy spider went up the spout again.
+"""
+
+def song(bug, adjective1, noun1, noun2, adjective2, noun3, adjective3):
+    print("/n/n")
+    print(f"The itsy bitsy {bug}")
+    print(f"{adjective1} up the {noun1} spout.")
+    print(f"Down came the {noun2},")
+    print(f"and {adjective2} the {bug} out.")
+    print(f"Out came the {noun3},")
+    print(f"and dried up all the {noun2},")
+    print(f"and the itsy bitsy {bug} {adjective3} up the spout again.")
+
+input_bug = input("Enter a type of bug:  ") #spider
+input_adjective1 = input("Enter an adjective:  ")#crawled
+input_noun1 = input("Enter a noun:  ")#water
+input_noun2 = input("Enter another noun:  ")#rain
+input_adjective2 = input("Enter another adjective:  ")#washed
+input_noun3 = input("Enter another noun:  ")#sun
+input_adjective3 = input("Enter another adjective:  ")#went
+
+song(bug=input_bug, adjective1=input_adjective1, noun1=input_noun1, noun2=input_noun2, adjective2=input_adjective2, noun3=input_noun3, adjective3=input_adjective3 )
