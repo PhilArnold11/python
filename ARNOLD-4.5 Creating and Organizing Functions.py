aed003bc568be33e95e0efe699049b4b9e95315d
PhilArnold11 <157064792+PhilArnold11@users.noreply.github.com>
2024-02-29 20:00:08 -0600

    Add files via upload

---
+++ ARNOLD-4.5 Creating and Organizing Functions.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    if n == 1 or n == 0:#define base case
+        return 1
+    else:#define recursive step
+        return n*factorial(n-1)#n multiplied by a call to itself with n-1
+def main():
+    n = int(input('Enter a non-negative integer:  '))#prompt the user to enter non-negative integer
+    factorial(n)#call the factorial function
+    print (f'The factorial of {n} is {factorial(n)}')#use the users input as argument to factorial function
+main()#call main function to run program
