a7e2d7854a42de9c606b4111d30fe98af16f6795
PhilArnold11 <157064792+PhilArnold11@users.noreply.github.com>
2024-03-03 17:02:12 -0600

    Add files via upload

--- ARNOLD-4.6B Dictionaries.py
+++ ARNOLD-4.6B Dictionaries.py
@@ -1,7 +1,13 @@
-nato_alphabet = {'A':'Alpha', " ":" ", 'B':'Bravo', 'C':'Charlie', 'D':'Delta', 'E':'Echo', 'F':'Fox-trot', 'G':'Golf', 'H':'Hotel', 'I':'Indigo', 'J':'Juliet', 'K':'Kilo', 'L':'Lima', 'M':'Mike', 'N':'November', 'O':'Oscar', 'P':'Papa', 'Q':'Quebec', 'R':'Romeo', 'S':'Sierra', 'T':'Tango', 'U':'Uniform', 'V':'Victor', 'W':'Whiskey', 'X':'X-ray', 'Y':'Yankee', 'Z':'Zulu'}
+nato_alphabet = {'A':'Alpha', " ":" ", 'B':'Bravo', 'C':'Charlie', 'D':'Delta', 'E':'Echo', 'F':'Foxtrot', 'G':'Golf', 'H':'Hotel', 'I':'India', 'J':'Juliett', 'K':'Kilo', 'L':'Lima', 'M':'Mike', 'N':'November', 'O':'Oscar', 'P':'Papa', 'Q':'Quebec', 'R':'Romeo', 'S':'Sierra', 'T':'Tango', 'U':'Uniform', 'V':'Victor', 'W':'Whiskey', 'X':'X-ray', 'Y':'Yankee', 'Z':'Zulu'}
 
 def spell_word():
     user_word = input('Enter any word: ')
-    user_word=user_word.capitalize()
-    print(user_word)
+    uppercase_text = user_word.upper() #I couldn't figure this out for some reason. I googled it and used the results from this page https://flexiple.com/python/python-uppercase
+    for letter in uppercase_text:
+        if letter in nato_alphabet:
+        print()
 spell_word()
+
+def main():
+    spell_word()
+main()
