5d671994142927c2a232aac2e2c3188a02b6b69f
PhilArnold11 <157064792+PhilArnold11@users.noreply.github.com>
2024-03-03 19:09:53 -0600

    Update ARNOLD-4.7-Exceptions.py

--- ARNOLD-4.7-Exceptions.py
+++ ARNOLD-4.7-Exceptions.py
@@ -1,12 +1,9 @@
-nato_alphabet = {'A':'Alpha', " ":" ", 'B':'Bravo', 'C':'Charlie', 'D':'Delta', 'E':'Echo', 'F':'Foxtrot', 'G':'Golf', 'H':'Hotel', 'I':'India', 'J':'Juliett', 'K':'Kilo', 'L':'Lima', 'M':'Mike', 'N':'November', 'O':'Oscar', 'P':'Papa', 'Q':'Quebec', 'R':'Romeo', 'S':'Sierra', 'T':'Tango', 'U':'Uniform', 'V':'Victor', 'W':'Whiskey', 'X':'X-ray', 'Y':'Yankee', 'Z':'Zulu'}
-#created dictionary using NATO alphabet
-def spell_word():#define function
-    user_word = input('Enter any word: ')#ask user for input
-    for letter in user_word:
-        letter = letter.upper()#convert string to uppercase
-        if letter in nato_alphabet:
-            print(nato_alphabet[letter])#if the letter is found the NATO definition is printed out
+def square_number():
+    try:#add exception handling
+        number = input("Enter a number to square: ")
+        squared_number = int(number) ** 2
+        print(f"The square of {number} is {squared_number}.")
+    except ValueError:#add exception handling
+        print("Please enter an integer.")
 
-def main():#encapsulate in main function and call spell word
-    spell_word()
-main()
+square_number()
