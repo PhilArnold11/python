851f9427bce6b2a78307decf2615787e9be96a4e
PhilArnold11 <157064792+PhilArnold11@users.noreply.github.com>
2024-03-14 11:29:13 -0500

    Add files via upload

---
+++ ARNOLD-2-2.4-Strings in Action.py
@@ -0,0 +1,13 @@
+def main():
+    titles = []
+    new_titles = ''
+    while new_titles != 'done':
+            new_titles = input('Please enter the title of 10 books:')
+            titles.append(new_titles)
+            print(titles)
+
+
+
+main()
+
+#http://introtopython.org/while_input.html
