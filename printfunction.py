a3f930591125e77247fc0cb7faec5210a072918b
PhilArnold11 <157064792+PhilArnold11@users.noreply.github.com>
2024-10-01 18:29:32 -0500

    Add files via upload

---
+++ printfunction.py
@@ -0,0 +1,9 @@
+# knock-knock joke program
+print("Knock, knock.")
+print("Who's there?")
+print("Beets.")
+print("Beets who?")
+print("Beets me, go find out for yourself.")
+
+# as one line of code using escape characters
+print("Knock, knock.\nWho's there?\nBeets.\nBeets who?\nBeets me, go find out for yourself.")
