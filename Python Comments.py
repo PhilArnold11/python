6d44d4f2f4b76c9f69602e595ce1d92909dabb3b
PhilArnold11 <157064792+PhilArnold11@users.noreply.github.com>
2024-10-10 09:00:36 -0500

    Add files via upload

---
+++ Python Comments.py
@@ -0,0 +1,9 @@
+# This program calculates how many miles per gallon per tank.
+
+#capacity of tank size
+tank_size = 15
+#range per tank
+distance = 600
+#calculation for miles per gallon
+mpg = distance / tank_size
+print("Miles per gallon:", mpg)
